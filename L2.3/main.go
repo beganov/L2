ea1031f80cf3cf0806e73846777d25487cdabe71
beganov <beganovdv@gmail.com>
2025-10-13 21:26:41 +0300

    L2.2 done

---
+++ L2.3/main.go
@@ -0,0 +1,22 @@
+// L2.3
+// Что выведет программа?
+// nil
+// false
+// Объяснить внутреннее устройство интерфейсов и их отличие от пустых интерфейсов.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func Foo() error {
+	var err *os.PathError = nil
+	return err
+}
+
+func main() {
+	err := Foo()
+	fmt.Println(err)
+	fmt.Println(err == nil)
+}
